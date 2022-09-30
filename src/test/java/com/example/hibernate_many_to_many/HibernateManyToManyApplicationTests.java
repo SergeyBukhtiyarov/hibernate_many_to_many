86d8afc4a9cf789f27a6557bcd0cadbc4d1ff4b9
SergeyBukhtiyarov <bukhtiyarov.s.a@gmail.com>
2022-09-30 15:09:37 +0400

    project begins

---
+++ src/test/java/com/example/hibernate_many_to_many/HibernateManyToManyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hibernate_many_to_many;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateManyToManyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
